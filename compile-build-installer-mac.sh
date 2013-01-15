2edcdb406ef13b9b3a79a2e398d0943600988a9f
FLORIAN PERVES <florian@pynbm.org>
2013-01-15 10:14:06 +0100

    Revert "PYNBM-1.1.1"
    This reverts commit 2b59fa506e6919ebe0d804644f16cd54648e43c8.

--- compile-build-installer-mac.sh
+++
@@ -1,2 +0,0 @@
-python py2app_setup.py py2app
-
--- compile-build-installer.ps1
+++
@@ -1,7 +0,0 @@
-$version = get-content PYNBM.py | %{ if ($_.Split('"')[0] -match "^Version") { $_.Split('"')[1]; } }
-
-invoke-expression "python setup.py py2exe"
-$nsiscmd = 'c:\Program Files (x86)\NSIS\makensis.exe'
-$nsisopt = '/DVERSION='+$version
-$nsisopt2 = 'installer.nsi'
-& $nsiscmd $nsisopt $nsisopt2
